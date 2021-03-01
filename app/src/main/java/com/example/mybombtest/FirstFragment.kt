c640fa75f656cbb66aeb2bfd425781f724cad7f8
spamshot <kylan.hill@gmail.com>
2021-02-28 21:19:51 -0500

    bbb

--- app/src/main/java/com/example/mybombtest/FirstFragment.kt
+++ app/src/main/java/com/example/mybombtest/FirstFragment.kt
@@ -24,7 +24,6 @@ class FirstFragment : Fragment() {
 
     val defuseCode = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) //Numbers for defuseCode
 
-
     var mediaPlayerSiren: MediaPlayer? = null //inside clear BTN
     var mediaPlayerBeeper: MediaPlayer? = null //inside Timer
     var mediaPlayerBoomBombSound: MediaPlayer? = null //inside end Timer
